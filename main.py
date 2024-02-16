d005738fbd0c5ed50306213894f4320b57cb9d65
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-02-17 07:25:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -129,8 +129,8 @@ def merge_clash(configs:List[str]) -> str:
             # 如果代理节点的名称中包含 ">"，则跳过该节点
             if ">" in proxy['name']:
                 continue
-            #if "2022-blake3-chacha20" in proxy['-cipher']:
-                #continue
+            if "2022-blake3-chacha20-poly1305" in proxy['cipher']:
+                continue
             # 测试代理节点是否有效，如果无效，则跳过该节点
             if "vless" in proxy['type']:
                 continue
@@ -141,7 +141,7 @@ def merge_clash(configs:List[str]) -> str:
             proxy['name'] = proxy['name'] + f'_{i}@{j}'
             # 将代理节点添加到列表中
             proxies.append(proxy)
-            print(proxies)
+            #print(proxies)
     # 获取所有代理节点的名称列表
     node_names:List[str] = list(map(lambda n: n['name'], proxies))
     # 将所有代理节点添加到配置模板中
